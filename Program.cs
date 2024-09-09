0637e3ba2b464f9c3e446d48a63f096fb819c83d
Larissa dos Santos <laridsantoslima@gmail.com>
2024-09-08 23:44:30 -0300

    Desafio DIO POO Sistema de Smartphones CONCLUÍDO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+var nokia = new Nokia("1234", "Nokia", "Imei-432", 986547);
+
+Console.WriteLine("Smartphone Nokia:");
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+
+var iphone = new Iphone("9999", "Iphone", "Imei-2233", 843721);
+
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
